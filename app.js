54954a983cc901a4834a0b4a5489988c90d67581
Kagen Chow <kc@email.com>
2023-03-08 17:30:46 +0800

    Routing + Controller

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const dotenv = require('dotenv');
 //Setting up config.env file vars
 dotenv.config({path: "./config.env"})
 
+//Importing all routes
+const jobs = require('./routes/jobs');
+
+app.use('/api/v1', jobs);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
