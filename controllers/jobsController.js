29a3ae57e1624fef91370df2b3694de71c18fced
Kagen Chow <kc@email.com>
2023-03-09 17:14:23 +0800

    Update & Delete Jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,7 @@
 const Job = require('../models/jobs');
 
+const geoCoder = require('../utils/geocoder');
+
 //Get all Jobs => /api/v1/jobs
 exports.getJobs = async (req, res, next) => {
 
@@ -26,3 +28,68 @@ exports.newJob = async (req, res, next) => {
         data: job
     })
 }
+
+//Update a Job => /api/v1/job/:id
+exports.updateJob = async (req, res, next) => {
+    let job = await Job.findById(req.params.id);
+
+    if(!job) {
+        return res.status(404).json({
+            success: false,
+            message: 'Job not found.'
+        });
+    }
+
+    job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'Job is updated.',
+        data: job
+    });
+}
+
+exports.deleteJob = async (req, res, next) => {
+    let job = await Job.findById(req.params.id);
+
+    if (!job) {
+        return res.status(404).json({
+            success: false,
+            message: 'Job not found.'
+        })
+    }
+
+    job = await Job.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        message: 'Job is deleted.',
+        data: job
+    });
+}
+
+//Search jobs within radius => /api/v1/jobs/:zipcode/:distance
+exports.getJobsInRadius = async (req, res, next) => {
+    const { zipcode, distance } = req.params;
+
+    //Getting latitude & longitude from geocoder with zipcode
+    const loc = await geoCoder.geocode(zipcode);
+
+    const latitude = loc[0].latitude;
+    const longitude = loc[0].longitude;
+
+    const radius = distance / 3963;
+
+    const jobs = await Job.find({
+        location: { $geoWithin: { $centerSphere: [[longitude, latitude], radius] } }
+    });
+
+    res.status(200).json({
+        success: true,
+        results: jobs.length,
+        data: jobs
+    });
+};
--- routes/jobs.js
+++ routes/jobs.js
@@ -4,11 +4,18 @@ const router = express.Router();
 //Importing jobs controller methods
 const {
     getJobs,
-    newJob
+    newJob,
+    getJobsInRadius,
+    updateJob,
+    deleteJob
 } = require('../controllers/jobsController');
 
 router.route('/jobs').get(getJobs);
 
 router.route('/job/new').post(newJob);
+
+router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
+
+router.route('/job/:id').put(updateJob).delete(deleteJob);
 
 module.exports = router;
